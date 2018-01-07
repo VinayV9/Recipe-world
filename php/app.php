67f7c6d6860f9045e1606d6281c32ef5527f3cd9
VinayV9 <vinayveeren@gmail.com>
2018-01-07 22:03:34 +0530

    after long effort a responsive page i ever made:)

--- php/app.php
+++ php/app.php
@@ -1,12 +1,16 @@
 
 <?php
+
    $serverName= "localhost";
    $userName= "userName";
    $password= "password";
+   $db= "recipeworld";
 
    $connection= new mysqli($serverName, $userName, $password);
    if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
    } 
    echo "Connected successfully";
+
+   mysqli_select_db($db)
 ?>
--- php/register.php
+++ php/register.php
@@ -1,3 +1,19 @@
 <?php
-   
+   include "app.php";
+
+   if(isset($_POST['register'])){
+       $name= $_POST['name'];
+       $email= $_POST['email'];
+       $password= $_POST['password'];
+       $qry= "INSERT  INTO  `register`
+        ( `name`,
+          `email`,
+          `password`
+        ) VALUES 
+        (
+          '$name',
+          '$email',
+          '$password' 
+        ) ";  
+   }
 ?>
